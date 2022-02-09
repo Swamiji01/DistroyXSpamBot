955eda4a93e393d48b8cb214f0f02f64aa3e425e
NOBITA_XD <96421875+kuldiprathod@users.noreply.github.com>
2022-02-10 00:22:20 +0530

    Update help.py

--- HeartHacker/plugins/help.py
+++ HeartHacker/plugins/help.py
@@ -37,7 +37,7 @@ async def help(event):
         Button.url("ᴀʟʟ ᴄᴍᴅs", "https://telegra.ph/%F0%9D%97%A5%F0%9D%97%9C%F0%9D%97%AD%F0%9D%97%A2%F0%9D%97%98%F0%9D%97%9F-%F0%9D%97%AB-%F0%9D%97%A6%F0%9D%97%A3%F0%9D%97%94%F0%9D%97%A0-11-28-2")
         ],
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/YamlokOfficial")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/distroyspam")
         ] 
         ]
         )                                                         
